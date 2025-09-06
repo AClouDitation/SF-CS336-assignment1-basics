6814721a5f93c8e2224f8afbc9290ed8731d30a0
AClouD <25907312+AClouDitation@users.noreply.github.com>
2025-09-05 23:55:57 -0700

    add cross entropy

--- cs336_basics/transformer/utils.py
+++ cs336_basics/transformer/utils.py
@@ -15,6 +15,9 @@ def soft_max(x: Float[Tensor, " ..."], dim: int = -1) -> Float[Tensor, " ..."]:
     return x / x.sum(dim=dim, keepdim=True)
 
 
+def log_soft_max(x: Float[Tensor, " ..."], dim: int = -1) -> Float[Tensor, " ..."]:
+    return x - torch.log(x.exp().sum(dim=dim, keepdim=True))
+
 def scaled_dot_product_attention(
     q: Float[Tensor, "batch_size ... n d_k"],
     k: Float[Tensor, "batch_size ... m d_k"],
@@ -27,3 +30,12 @@ def scaled_dot_product_attention(
 
     t = pre_softmax / (k.shape[-1] ** 0.5)
     return einx.dot("... n m, ... m d_v -> ... n d_v", soft_max(t), v)
+
+
+def cross_entropy(
+    logits: Float[Tensor, "batch_size d_vocab"],
+    targets: Float[Tensor, "batch_size"],
+):
+    logits = logits - logits.max(dim=-1, keepdim=True).values
+    losses = -log_soft_max(logits).gather(dim=-1, index=targets.unsqueeze(-1))
+    return losses.mean(dim=0)
